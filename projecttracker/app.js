18bbd1ca88b0a11f99e6c41d0657382502b920b9
Himanshu Himanshu <200503981@student.georgianc.on.ca>
2023-06-12 22:47:16 -0400

    Second commit

--- projecttracker/app.js
+++ projecttracker/app.js
@@ -4,8 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Router object
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var aboutRouter = require('./routes/about');
+var projectsRouter = require('./routes/projects');
+var contactRouter = require('./routes/contact');
 
 var app = express();
 
@@ -19,8 +23,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routing configuration
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
+app.use('/projects', projectsRouter);
+app.use('/contact', contactRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
